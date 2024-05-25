c7fd5bec444e4614e72c7fd696f2723c67ec03fd
ws <18280296472@163.com>
2024-05-25 08:08:01 +0800

    添加list注释

--- kernel/include/list.h
+++ kernel/include/list.h
@@ -4,6 +4,11 @@
 #include <types.h>
 #include <compiler.h>
 
+/*
+ * These are non-NULL pointers that will result in page faults
+ * under normal circumstances, used to verify that nobody uses
+ * non-initialized list entries.
+ */
 #define LIST_POISON1 ((void *)0x100)
 #define LIST_POISON2 ((void *)0x122)
 
