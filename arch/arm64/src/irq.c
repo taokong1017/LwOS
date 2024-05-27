fcf91185f20cde8de7df8799dfcd671769e7abf5
ws <18280296472@163.com>
2024-05-28 00:24:01 +0800

    删除arm64_irq_priority_set

--- arch/arm64/src/irq.c
+++ arch/arm64/src/irq.c
@@ -8,10 +8,6 @@ void arch_irq_disable(uint32_t irq) { arm_gic_irq_disable(irq); }
 
 int arch_irq_is_enabled(uint32_t irq) { return arm_gic_irq_is_enabled(irq); }
 
-void z_arm64_irq_priority_set(uint32_t irq, uint32_t prio, uint32_t flags) {
-	arm_gic_irq_set_priority(irq, prio, flags);
-}
-
 int arch_irq_connect(uint32_t irq, uint32_t priority,
 					 void (*routine)(const void *), const void *parameter,
 					 uint32_t flags) {
@@ -20,7 +16,7 @@ int arch_irq_connect(uint32_t irq, uint32_t priority,
 	}
 
 	isr_install(irq, routine, parameter);
-	z_arm64_irq_priority_set(irq, priority, flags);
+	arm_gic_irq_set_priority(irq, priority, flags);
 
 	return 0;
 }
