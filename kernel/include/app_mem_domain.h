f8d12bef029925e1d843bd9dfe66a8cec75e22b8
ws <18280296472@163.com>
2024-10-07 11:16:36 +0800

    【新功能】：增加应用内存分区节，包括数据段和BSS段

---
+++ kernel/include/app_mem_domain.h
@@ -0,0 +1,31 @@
+#ifndef __APP_MEM_DOMAIN_H__
+#define __APP_MEM_DOMAIN_H__
+
+#include <types.h>
+#include <mem_domain.h>
+
+#define SECTION(segment) __attribute__((section(#segment)))
+#define APP_DATA(name) SECTION(.name##_data)
+#define APP_BSS(name) SECTION(.name##_bss)
+#define APP_DATA_START(name) __##name##_data_start
+#define APP_DATA_END(name) __##name##_data_end
+#define APP_BSS_START(name) __##name##_bss_start
+#define APP_BSS_END(name) __##name##_bss_end
+
+#define APP_PARTITION_DEFINE(name)                                             \
+	extern char APP_DATA_START(name)[];                                        \
+	extern char APP_DATA_END(name)[];                                          \
+	const struct mem_range name##_data_region = {                              \
+		.start = (void *)&APP_DATA_START(name)[0],                             \
+		.end = (void *)&APP_DATA_END(name)[0],                                 \
+		.attrs = PAGE_SHARED,                                                  \
+	};                                                                         \
+	extern char APP_BSS_START(name)[];                                         \
+	extern char APP_BSS_END(name)[];                                           \
+	const struct mem_range name##_bss_region = {                               \
+		.start = (void *)&APP_BSS_START(name)[0],                              \
+		.end = (void *)&APP_BSS_END(name)[0],                                  \
+		.attrs = PAGE_SHARED,                                                  \
+	};
+
+#endif
--- samples/kernel/task_006.c
+++ samples/kernel/task_006.c
@@ -2,8 +2,17 @@
 #include <task.h>
 #include <string.h>
 #include <mem_domain.h>
+#include <app_mem_domain.h>
+#include <menuconfig.h>
 
 static struct mem_domain app_mem_domain;
+APP_PARTITION_DEFINE(app_1);
+APP_DATA(app_1) int data = 1;
+APP_BSS(app_1) char array[4000] = {1};
+
+APP_PARTITION_DEFINE(app_2);
+APP_DATA(app_2) int data2 = 1;
+APP_BSS(app_2) char array2[8000] = {1};
 
 int main() {
 	printf("enter root task\n");
