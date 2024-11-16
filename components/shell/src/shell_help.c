9885388bc7b9c13bad445e99438e9a2f5f9c9671
ws <18280296472@163.com>
2024-11-16 16:43:00 +0800

    【新功能】：增加Shell子命令帮助信息输出基础接口

--- components/shell/src/shell_help.c
+++ components/shell/src/shell_help.c
@@ -5,6 +5,8 @@
 #include <shell_ops.h>
 #include <shell_util.h>
 
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 static void shell_formatted_text_print(struct shell *shell, const char *str,
 									   size_t terminal_offset,
 									   bool offset_first_line) {
@@ -100,6 +102,68 @@ void shell_help_cmd_print(struct shell *shell,
 	shell_show(shell, "%s%s", entry->syntax, cmd_sep);
 
 	shell_formatted_text_print(shell, entry->help, field_width, false);
+}
+
+static void help_item_print(struct shell *shell, const char *item_name,
+							uint32_t item_name_width, const char *item_help) {
+	static const char tabulator[] = "  ";
+	uint32_t offset = 2 * strlen(tabulator) + item_name_width + 1;
+	uint32_t space_num = 0;
+	uint32_t index = 0;
+	const char space = ' ';
+
+	if ((item_name == NULL) || (item_name[0] == '\0')) {
+		return;
+	}
+
+	shell_color_show(shell, SHELL_NORMAL, "%s%s", tabulator, item_name);
+
+	space_num = item_name_width - strlen(item_name);
+	if (item_help) {
+		for (index = 0; index < space_num; index++) {
+			shell_show(shell, "%c", space);
+		}
+	}
+
+	if (item_help == NULL) {
+		shell_cursor_next_line_move(shell);
+		return;
+	} else {
+		shell_color_show(shell, SHELL_NORMAL, "%s: ", tabulator);
+	}
+
+	shell_formatted_text_print(shell, item_help, offset, false);
+
+	return;
+}
+
+void shell_help_subcmd_print(struct shell *shell,
+							 const struct shell_entry *parent,
+							 const char *description) {
+	const struct shell_entry *entry = NULL;
+	uint32_t max_len = 0U;
+	uint32_t index = 0;
+
+	/* Search for the longest subcommand to print */
+	while ((entry = shell_cmd_get(parent, index++)) != NULL) {
+		max_len = max(max_len, strlen(entry->syntax));
+	}
+
+	if (max_len == 0U) {
+		return;
+	}
+
+	if (description != NULL) {
+		shell_color_show(shell, SHELL_NORMAL, description);
+	}
+
+	/* Print subcommands and help string */
+	index = 0;
+	while ((entry = shell_cmd_get(parent, index++)) != NULL) {
+		help_item_print(shell, entry->syntax, max_len, entry->help);
+	}
+
+	return;
 }
 
 bool shell_help_cmd_is_request(const char *str) {
