cfcaaae2d336f5af790579793d51a713723fbb50
ws <18280296472@163.com>
2024-12-03 22:23:57 +0800

    【新功能】：调整Shell串口中断/轮询实现框架

--- components/shell/src/shell_uart.c
+++ components/shell/src/shell_uart.c
@@ -1,20 +1,36 @@
 #include <shell.h>
 
-static bool shell_uart_init(struct shell_transport *transport);
-static bool shell_uart_deinit(struct shell_transport *transport);
-static bool shell_uart_enable(struct shell_transport *transport, bool enable);
-static int32_t shell_uart_write(struct shell_transport *transport,
-								const char *data, uint32_t size);
-static int32_t shell_uart_read(struct shell_transport *transport, char *data,
-							   uint32_t size);
+#ifdef CONFIG_SHELL_UART_IRQ_TYPE
+static int32_t shell_uart_irq_write(struct shell_transport *transport,
+									const char *data, uint32_t size) {
+	(void)transport;
+	(void)data;
 
-const struct shell_transport_ops uart_transport_ops = {
-	.init = shell_uart_init,
-	.deinit = shell_uart_deinit,
-	.enable = shell_uart_enable,
-	.read = shell_uart_read,
-	.write = shell_uart_write,
-};
+	return size;
+}
+static int32_t shell_uart_irq_read(struct shell_transport *transport,
+								   char *data, uint32_t size) {
+	(void)transport;
+	(void)data;
+
+	return size;
+}
+#else
+static int32_t shell_uart_poll_write(struct shell_transport *transport,
+									 const char *data, uint32_t size) {
+	(void)transport;
+	(void)data;
+
+	return size;
+}
+static int32_t shell_uart_poll_read(struct shell_transport *transport,
+									char *data, uint32_t size) {
+	(void)transport;
+	(void)data;
+
+	return size;
+}
+#endif
 
 static bool shell_uart_init(struct shell_transport *transport) {
 	(void)transport;
@@ -37,19 +53,29 @@ static bool shell_uart_enable(struct shell_transport *transport, bool enable) {
 
 static int32_t shell_uart_write(struct shell_transport *transport,
 								const char *data, uint32_t size) {
-	(void)transport;
-	(void)data;
-
-	return size;
+#ifdef CONFIG_SHELL_UART_IRQ_TYPE
+	return shell_uart_irq_write(transport, data, size);
+#else
+	return shell_uart_poll_write(transport, data, size);
+#endif
 }
 
 static int32_t shell_uart_read(struct shell_transport *transport, char *data,
 							   uint32_t size) {
-	(void)transport;
-	(void)data;
-
-	return size;
+#ifdef CONFIG_SHELL_UART_IRQ_TYPE
+	return shell_uart_irq_read(transport, data, size);
+#else
+	return shell_uart_poll_read(transport, data, size);
+#endif
 }
+
+const struct shell_transport_ops uart_transport_ops = {
+	.init = shell_uart_init,
+	.deinit = shell_uart_deinit,
+	.enable = shell_uart_enable,
+	.read = shell_uart_read,
+	.write = shell_uart_write,
+};
 
 const struct shell_transport_ops *shell_uart_transport_ops_get() {
 	return &uart_transport_ops;
