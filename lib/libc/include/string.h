ae18e82a76dbd252bf289c50de2e77ac086da2d5
ws <18280296472@163.com>
2025-01-23 22:31:04 +0800

    【代码优化】：删除关键字restict

--- lib/libc/include/string.h
+++ lib/libc/include/string.h
@@ -7,19 +7,19 @@
 extern "C" {
 #endif
 
-void *memcpy(void *__restrict, const void *__restrict, size_t);
+void *memcpy(void *, const void *, size_t);
 void *memmove(void *, const void *, size_t);
 void *memset(void *, int, size_t);
 int memcmp(const void *, const void *, size_t);
 void *memchr(const void *, int, size_t);
-char *strcpy(char *__restrict, const char *__restrict);
-char *strncpy(char *__restrict, const char *__restrict, size_t);
-char *strcat(char *__restrict, const char *__restrict);
-char *strncat(char *__restrict, const char *__restrict, size_t);
+char *strcpy(char *, const char *);
+char *strncpy(char *, const char *, size_t);
+char *strcat(char *, const char *);
+char *strncat(char *, const char *, size_t);
 int strcmp(const char *, const char *);
 int strncmp(const char *, const char *, size_t);
 int strcoll(const char *, const char *);
-size_t strxfrm(char *__restrict, const char *__restrict, size_t);
+size_t strxfrm(char *, const char *, size_t);
 char *strchr(const char *, int);
 char *strrchr(const char *, int);
 size_t strcspn(const char *, const char *);
@@ -27,14 +27,14 @@ size_t strspn(const char *, const char *);
 char *strpbrk(const char *, const char *);
 char *strstr(const char *, const char *);
 size_t strlen(const char *);
-char *strtok_r(char *__restrict, const char *__restrict, char **__restrict);
-char *stpcpy(char *__restrict, const char *__restrict);
-char *stpncpy(char *__restrict, const char *__restrict, size_t);
+char *strtok_r(char *, const char *, char **);
+char *stpcpy(char *, const char *);
+char *stpncpy(char *, const char *, size_t);
 size_t strnlen(const char *, size_t);
 char *strdup(const char *);
 char *strndup(const char *, size_t);
 void *memmem(const void *, size_t, const void *, size_t);
-void *memccpy(void *__restrict, const void *__restrict, int, size_t);
+void *memccpy(void *, const void *, int, size_t);
 char *strsep(char **, const char *);
 size_t strlcat(char *, const char *, size_t);
 size_t strlcpy(char *, const char *, size_t);
--- lib/libc/string/memcpy.c
+++ lib/libc/string/memcpy.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-void *memcpy(void *restrict dest, const void *restrict src, size_t n) {
+void *memcpy(void *dest, const void *src, size_t n) {
 	unsigned char *d = dest;
 	const unsigned char *s = src;
 
