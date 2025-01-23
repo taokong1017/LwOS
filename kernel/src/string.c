42a64e7953102572670eff60778a17d537d0ee9b
ws <18280296472@163.com>
2025-01-23 22:37:24 +0800

    【新功能】：增加libc库memset函数

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -103,22 +103,6 @@ size_t strnlen(const char *s, size_t count) {
 }
 
 /**
- * memset - Fill a region of memory with the given value
- * @s: Pointer to the start of the area.
- * @c: The byte to fill the area with
- * @count: The size of the area.
- *
- * Do not use memset() to access IO space, use memset_io() instead.
- */
-void *memset(void *s, int c, size_t count) {
-	char *xs = s;
-
-	while (count--)
-		*xs++ = c;
-	return s;
-}
-
-/**
  * memset64() - Fill a memory area with a uint64_t
  * @s: Pointer to the start of the area.
  * @v: The value to fill the area with
