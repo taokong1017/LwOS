b86725d245c1dea977e04aaddc20ca0f52ed05e3
ws <18280296472@163.com>
2025-02-04 20:42:24 +0800

    【新功能】：增加libc库strcmp函数

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -9,25 +9,6 @@ size_t strlen(const char *s) {
 }
 
 /**
- * strcmp - Compare two strings
- * @cs: One string
- * @ct: Another string
- */
-int strcmp(const char *cs, const char *ct) {
-	unsigned char c1, c2;
-
-	while (1) {
-		c1 = *cs++;
-		c2 = *ct++;
-		if (c1 != c2)
-			return c1 < c2 ? -1 : 1;
-		if (!c1)
-			break;
-	}
-	return 0;
-}
-
-/**
  * strncmp - Compare two length-limited strings
  * @cs: One string
  * @ct: Another string
