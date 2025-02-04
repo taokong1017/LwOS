d7e4408eec0ade4aa3ef7d0a2cdd06ebc66e2a29
ws <18280296472@163.com>
2025-02-04 20:15:01 +0800

    【新功能】：增加libc库strncat函数

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -1,21 +1,5 @@
 #include <string.h>
 
-char *strncat(char *dest, const char *src, size_t count) {
-	char *tmp = dest;
-
-	if (count) {
-		while (*dest)
-			dest++;
-		while ((*dest++ = *src++) != 0) {
-			if (--count == 0) {
-				*dest = '\0';
-				break;
-			}
-		}
-	}
-	return tmp;
-}
-
 size_t strlen(const char *s) {
 	const char *sc;
 
