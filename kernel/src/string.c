8232c648d39156a1bb23c0ee4bf72c2761d8d4ae
ws <18280296472@163.com>
2025-02-05 22:48:14 +0800

    【新功能】：增加libc库strstr函数

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -33,24 +33,3 @@ void *memset64(uint64_t *s, uint64_t v, size_t count) {
 		*xs++ = v;
 	return s;
 }
-
-/**
- * strstr - Find the first substring in a %NUL terminated string
- * @s1: The string to be searched
- * @s2: The string to search for
- */
-char *strstr(const char *s1, const char *s2) {
-	size_t l1, l2;
-
-	l2 = strlen(s2);
-	if (!l2)
-		return (char *)s1;
-	l1 = strlen(s1);
-	while (l1 >= l2) {
-		l1--;
-		if (!memcmp(s1, s2, l2))
-			return (char *)s1;
-		s1++;
-	}
-	return NULL;
-}
