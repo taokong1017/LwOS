585c407736e789a667c5098ba1b45dc9e4c2671e
ws <18280296472@163.com>
2025-02-06 22:04:38 +0800

    【新功能】：增加libc库strdup函数

---
+++ lib/libc/include/stdlib.h
@@ -0,0 +1,17 @@
+#ifndef __STDLIB_H__
+#define __STDLIB_H__
+
+#include <types.h>
+#include <mem_mgr.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define malloc mem_malloc
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
