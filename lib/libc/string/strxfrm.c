946d6a9086bb393c3d3941f0fbdcad5ddfe309c0
ws <18280296472@163.com>
2025-02-11 23:48:55 +0800

    【新功能】：增加libc库memccpy函数

--- lib/libc/string/strxfrm.c
+++ lib/libc/string/strxfrm.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-size_t strxfrm(char *restrict dest, const char *restrict src, size_t n)
+size_t strxfrm(char *dest, const char *src, size_t n)
 {
 	size_t l = strlen(src);
 	if (n > l) strcpy(dest, src);
