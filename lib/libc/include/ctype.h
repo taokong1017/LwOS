32c0aed47f5965a16b6f2909e0393a5defd4f8ed
ws <18280296472@163.com>
2025-02-28 21:04:38 +0800

    【代码优化】：重构ctype相关函数

---
+++ lib/libc/include/ctype.h
@@ -0,0 +1,21 @@
+#ifndef __CTYPE_H__
+#define __CTYPE_H__
+
+int isalnum(int);
+int isalpha(int);
+int isblank(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+int tolower(int);
+int toupper(int);
+int isascii(int);
+int toascii(int);
+
+#endif
